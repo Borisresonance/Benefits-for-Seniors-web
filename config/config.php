21470d482d33957aed814c8b1dfcaa77240ef176
juan diego llorente orozco <llorentejuandiego@gmail.com>
2024-04-05 21:48:55 -0500

    test

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('BASE_DIR', dirname(__FILE__));
+define('ASSETS_DIR', BASE_DIR . '/assets');
+define('IMAGES_DIR', ASSETS_DIR . '/images'); // Define path to images folder
+
+// Rest of your configuration...
